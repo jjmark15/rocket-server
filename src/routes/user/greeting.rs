b35610c6cfc045f92e0b62c385bf8c4720c581ff
Josh Jones <ohblonddev@gmail.com>
2019-10-05 22:41:00 +0100

    add create user route

--- src/routes/user/greeting.rs
+++ src/routes/user/greeting.rs
@@ -1,5 +1,14 @@
 
+use rocket_contrib::json::{Json};
+
+use crate::models::users::user::{User};
+
 #[get("/")]
 pub fn hello_world() -> &'static str {
     "Hello, world!"
+}
+
+#[post("/create", data = "<user>")]
+pub fn create(user: Json<User>) -> Json<User> {
+    user
 }
