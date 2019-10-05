cbe87eff81a456dcf254ae495264a95ca3a04316
Josh Jones <ohblonddev@gmail.com>
2019-10-05 22:41:07 +0100

    mount create route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
 
 mod routes;
 mod models;
 
 fn main() {
     rocket::ignite()
-    .mount("/", routes![routes::user::greeting::hello_world])
+    .mount("/api/user", routes![
+        routes::user::greeting::hello_world,
+        routes::user::greeting::create,
+        ])
     .launch();
 }
